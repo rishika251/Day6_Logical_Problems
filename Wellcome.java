b987d9cbcd89a68624d6f0a3fe010c3fc86c6280
rishika251 <rishikasadineni@gmail.com>
2022-11-09 11:02:15 +0530

    Added:wellcome

---
+++ Wellcome.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.Day6;
+
+public class Wellcome {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Wellcome toLogical problems");
+	}
+
+}
+
+
